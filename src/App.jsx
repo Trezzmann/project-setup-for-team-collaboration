4b32ee28c4443efd23f417a8267b826621d18113
Trezzmann <anahimana3@gmail.com>
2023-04-05 16:40:23 +0200

    fix: configure eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react';
 import reactLogo from './assets/react.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
 	const [count, setCount] = useState(0);
-	var a = 0;
+
 	return (
 		<div className="App">
 			<div>
