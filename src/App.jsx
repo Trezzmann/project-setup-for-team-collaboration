46de692d61323cbfed68d7d471d354e7e7c72a68
Trezzmann <anahimana3@gmail.com>
2023-04-05 14:49:55 +0200

    refactor: changes eslint configuration file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
@@ -10,9 +9,9 @@ function App() {
 		<div className="App">
 			<div>
 				<a href="https://vitejs.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
+					<img src="/vite.svg" className="logo" alt="Vite logo" />
 				</a>
-				<a href="https://reactjs.org" target="_blank">
+				<a href="https://reactjs.org" target="_blank" rel="noreferrer">
 					<img
 						src={reactLogo}
 						className="logo react"
@@ -22,7 +21,7 @@ function App() {
 			</div>
 			<h1>Vite + React</h1>
 			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
+				<button type="button" onClick={() => setCount((x) => x + 1)}>
 					count is {count}
 				</button>
 				<p>
